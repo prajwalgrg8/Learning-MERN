8b860adc0b53a76fc75892eebdf349b917598771
Prajwal Gurung <169533464+prajwalgrg8@users.noreply.github.com>
2026-09-07 09:27:39 +0545

    Node js Path module

---
+++ nodejs/modules/path.js
@@ -0,0 +1,12 @@
+import path from 'path';
+
+const filePath = "folder1/hello/hello.txt";
+
+console.log(path.dirname(filePath)); //folder name
+
+console.log(path.basename(filePath)); //file name
+
+console.log(path.extname(filePath)); //extension name
+
+
+
