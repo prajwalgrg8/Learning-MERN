80fdd2cc0aa8fb731fe1fb7205dea44ebdb7b23a
Prajwal Gurung <169533464+prajwalgrg8@users.noreply.github.com>
2026-09-07 09:31:35 +0545

    Node js url module

---
+++ nodejs/modules/url.js
@@ -0,0 +1,9 @@
+const codeitUrl = "https://www.codeit.com.np";
+
+const urlObject = new URL(codeitUrl);
+
+console.log(urlObject);
+
+console.log(urlObject.host);
+console.log(urlObject.protocol);
+console.log(urlObject.pathname);
